4419364ef978b046cf1322f45be7d46013db9d85
siporqueno <strike1000001@gmail.com>
2021-04-30 12:48:39 +0300

    Homework of Lesson 5. Added listing of cart items, cart subtotal, order total to view "register". Finally the way to placing order is open!

--- shop-ui/src/main/java/com/porejemplo/controller/OrderController.java
+++ shop-ui/src/main/java/com/porejemplo/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.porejemplo.controller;
 
+import com.porejemplo.service.CartService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +15,17 @@ public class OrderController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
 
+    private final CartService cartService;
+
+    @Autowired
+    public OrderController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
     @GetMapping
-    public String listOrderPage(){
+    public String listOrderPage(Model model) {
+        model.addAttribute("cartItems", cartService.getLineItems());
+        model.addAttribute("subTotal", cartService.calculateCartSubTotal());
         return "register";
     }
 }
