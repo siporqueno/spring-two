3ddfeec6d2f82d8676d3ba2e4f8674d3d6c79f9b
siporqueno <strike1000001@gmail.com>
2021-05-06 12:48:01 +0300

    Homework of Lesson 5. Added deserializer from String to BigDecimal to module in objectMapper in SessionConfig.

--- shop-ui/src/main/java/com/porejemplo/SessionConfig.java
+++ shop-ui/src/main/java/com/porejemplo/SessionConfig.java
@@ -1,6 +1,8 @@
 package com.porejemplo;
 
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.beans.factory.BeanClassLoaderAware;
@@ -12,6 +14,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 @Profile("!dev")
@@ -31,6 +34,12 @@ public class SessionConfig implements BeanClassLoaderAware {
 
         SimpleModule module = new SimpleModule();
         module.addSerializer(BigDecimal.class, new ToStringSerializer());
+       module.addDeserializer(BigDecimal.class, new FromStringDeserializer<BigDecimal>(String.class) {
+           @Override
+           protected BigDecimal _deserialize(String s, DeserializationContext deserializationContext) throws IOException {
+               return new BigDecimal(s);
+           }
+       });
         mapper.registerModule(module);
 
         return mapper;
