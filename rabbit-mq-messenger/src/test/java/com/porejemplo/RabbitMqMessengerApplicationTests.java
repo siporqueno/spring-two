c3b49903e0d4cc603a20b11780facd9ccbd97637
siporqueno <strike1000001@gmail.com>
2021-05-17 21:54:13 +0300

    Task 2 of Homework of Lesson 6. Repeated Classwork concerning RabbitMQ.

---
+++ rabbit-mq-messenger/src/test/java/com/porejemplo/RabbitMqMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
