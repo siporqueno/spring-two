bf7b0c5925ceac4db689879049e17af6ea0074fc
siporqueno <strike1000001@gmail.com>
2021-05-18 17:23:33 +0300

    Task 2 of Homework of Lesson 6. Configured Beans "rabbitTemplate" etc. in ShopUiApplication. Used rabbitTemplate in OrderServiceImpl.createOrder() to send textMessage to queue "shop.queue" with routing key "shop". The class TextMessage was moved to module shop-common. The text message contains order id and order value. It is read by the receiver in module rabbit-mq-messenger from Classwork.

--- shop-ui/src/main/java/com/porejemplo/ShopUiApplication.java
+++ shop-ui/src/main/java/com/porejemplo/ShopUiApplication.java
@@ -1,17 +1,54 @@
 package com.porejemplo;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @SpringBootApplication
 public class ShopUiApplication {
 
     @Bean
+    Queue shopQueue() {
+        return new Queue("shop.queue", false);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange("users.exchange");
+    }
+
+    @Bean
+    Binding firstBinding(Queue shopQueue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(shopQueue)
+                .to(exchange)
+                .with("shop"); // routing key
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     public static void main(String[] args) {
